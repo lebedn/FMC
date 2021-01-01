5f317331de1256c5293f57a2401d2b0ecba7386d
Natala <nata.lebed.1998@gmail.com>
2021-01-01 18:30:32 +0200

    section four

--- src/js/script.js
+++ src/js/script.js
@@ -9,9 +9,6 @@ $(document).ready(function() {
         dots: true,
         adaptiveHeight: true,
         speed: 600
-
-
-
     });
     new WOW().init();
 });
@@ -19,9 +16,4 @@ $('ul.experimental_solutions__tabs').on('click', 'li:not(.experimental_solutions
     $(this)
         .addClass('experimental_solutions__tab_active').siblings().removeClass('experimental_solutions__tab_active')
         .closest('div.container').find('div.experimental_solutions__content').removeClass('experimental_solutions__content_active').eq($(this).index()).addClass('experimental_solutions__content_active');
-});
-$(document).ready(function() {
-    $('.your-class').slick({
-
-    });
 });
